346c5da7f94dfeeea8cb2424a2fd90df552a2094
Diogo Costa <46428002+diogo0254jf@users.noreply.github.com>
2023-05-30 18:32:01 -0300

    atualizado a API para receber requisições

--- src/main/java/com/diogo/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/diogo/helpdesk/resources/TecnicoResource.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -34,34 +33,35 @@ public class TecnicoResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(new TecnicoDTO(obj));
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
     @GetMapping
     public ResponseEntity<List<TecnicoDTO>> findAll() {
         List<Tecnico> list = service.findAll();
-        List<TecnicoDTO> listdto = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
-        return ResponseEntity.status(HttpStatus.OK).body(listdto);
+        List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
     @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<Tecnico> create(@Valid @RequestBody TecnicoDTO objDTO) {
-        objDTO.setId(null);
+    public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO) {
         Tecnico newObj = service.create(objDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
-        return ResponseEntity.created(uri).body(newObj);
+        return ResponseEntity.created(uri).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping(value = "/{id}")
-    public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @RequestBody TecnicoDTO objDTO) {
+    public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @Valid @RequestBody TecnicoDTO objDTO) {
         Tecnico obj = service.update(id, objDTO);
-        return ResponseEntity.status(HttpStatus.OK).body(new TecnicoDTO(obj));
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Integer> delete(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id) {
         service.delete(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(id);
+        return ResponseEntity.noContent().build();
     }
 }
